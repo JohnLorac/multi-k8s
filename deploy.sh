448d5ea4df0bbf23bdea116e2cff6431f482b62d
JohnLorac <johnlorac@gmail.com>
2019-05-02 13:42:02 +0200

    243. Updating the Deployment Script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t johnlorac/multi-client -f ./client/Dockerfile ./client
-docker build -t johnlorac/mutli-server -f ./server/Dockerfile ./server
-docker build -t johnlorac/multi-worker -f ./worker/Dockerfile ./worker
-docker push johnlorac/multi-client
-docker push johnlorac/multi-server
-docker push johnlorac/multi-worker
+docker build -t johnlorac/multi-client:latest -t johnlorac/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t johnlorac/mutli-server:latest -t johnlorac/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t johnlorac/multi-worker:latest -t johnlorac/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push johnlorac/multi-client:latest
+docker push johnlorac/multi-server:latest
+docker push johnlorac/multi-worker:latest
+
+docker push johnlorac/multi-client:$SHA
+docker push johnlorac/multi-server:$SHA
+docker push johnlorac/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=johnlorac/multi-server
+kubectl set image deployments/server-deployment server=johnlorac/multi-server:$SHA
+kubectl set image deployments/client-deployment client=johnlorac/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=johnlorac/multi-worker:$SHA
